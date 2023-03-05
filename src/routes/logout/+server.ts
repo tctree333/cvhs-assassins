49b000653182c595bf65a81ebe50e44bce4a1a0d
Tomi Chen <tctree333@users.noreply.github.com>
2023-03-05 10:45:46 -0800

    use sveltekit cookie methods

--- src/routes/logout/+server.ts
+++ src/routes/logout/+server.ts
@@ -1,11 +1,7 @@
+import { redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async () => {
-	return new Response(undefined, {
-		status: 302,
-		headers: {
-			location: '/',
-			'Set-Cookie': `session=""; Path=/; Max-Age=-1; HttpOnly; Secure; SameSite=Lax;` // 7 days
-		}
-	});
+export const GET: RequestHandler = async ({ cookies }) => {
+	cookies.delete('session', { path: '/' });
+	throw redirect(302, '/');
 };
